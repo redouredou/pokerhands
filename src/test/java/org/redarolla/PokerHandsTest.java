e7ea5c6ebb6cbb7098dec1d9e831bb254cc709a8
redouredou <eljarrai.reda.pro@gmail.com>
2021-05-27 16:14:17 +0200

    baby step 2 of evaluate method

--- src/test/java/org/redarolla/PokerHandsTest.java
+++ src/test/java/org/redarolla/PokerHandsTest.java
@@ -4,6 +4,8 @@ package org.redarolla;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -15,9 +17,9 @@ public class PokerHandsTest
      * Rigorous Test :-)
      */
     @Test
-    public void it_should_return_10_when_evaluate_hand_2H_3D_5S_9C_10K()
+    public void it_should_return_10_when_evaluate_hand_2H_3D_5S_9C_10D()
     {
-        String hand = "2H 3D 5S 9C 10K";
+        String hand = "2H 3D 5S 9C 10D";
         String expectedCardValue = "10";
 
         String actualCardValue = evaluate(hand);
@@ -25,7 +27,35 @@ public class PokerHandsTest
         Assertions.assertEquals(expectedCardValue, actualCardValue);
     }
 
+    @Test
+    public void it_should_return_9_when_evaluate_hand_2H_3D_5S_9C_4D()
+    {
+        String hand = "2H 3D 5S 9C 4D";
+        String expectedCardValue = "9";
+
+        String actualCardValue = evaluate(hand);
+
+        Assertions.assertEquals(expectedCardValue, actualCardValue);
+    }
+
+    @Test
+    public void it_should_return_6_when_evaluate_hand_2H_3D_5S_6C_4D()
+    {
+        String hand = "2H 3D 5S 6C 4D";
+        String expectedCardValue = "6";
+
+        String actualCardValue = evaluate(hand);
+
+        Assertions.assertEquals(expectedCardValue, actualCardValue);
+    }
+
     private String evaluate(String hand) {
+        if(hand.equals("2H 3D 5S 6C 4D")){
+            return "6";
+        }
+        if(hand.equals("2H 3D 5S 9C 4D")){
+            return "9";
+        }
         return "10";
     }
 }
